dd34aaf51a16c597ec2ea5db76f8414d142cde61
mj <1533836992@qq.com>
2023-03-15 15:27:00 +0800

    1.0......0

--- leetcode/test/CodeHuaWeiTest.java
+++ leetcode/test/CodeHuaWeiTest.java
@@ -1,44 +1,84 @@
 package leetcode.test;
 
+import java.util.Scanner;
+
 /**
  * 华为OD 机考 题目。卧槽还不如自己做，这些外包HR真的坑爹啊;
  */
 public class CodeHuaWeiTest {
 
     public static void main(String[] args) {
-//        String [] inputs = new String[] {"01","02"};
-        String[] inputs = new String[]{"01", "02", "11"};
-        getMin(inputs);
+        getMin();
 
-        userSetZero();
+//        userSetZero();
     }
 
-    public static void getMin(String[] strings) {
+    public static void getMin() {
+        //定义输入得数据
+        String[] strings = new String[]{};
+        //使用java得输入函数
+        Scanner in = new Scanner(System.in);
+        //定义分隔
+        in.useDelimiter("\n");//按回车分隔
+        //分隔获取输入得数据
+        strings = in.next().split(" ");
+
+        if (strings == null || strings.length == 0) System.out.println("");//输入为空 那么返回也为空
+
         //定义dp[i] 表示 i个字符的组成的最小值的string
         String[] dp = new String[strings.length + 1];
-        //
+        //初始化空字符组成的最小值也是“”
         dp[0] = "";
-        //规律
+        //规律:
+        //i个字符组成的最小值，一定是i-1个字符的最小值，然后和最后一个字符再去组合，然后比较
         for (int i = 1; i <= strings.length; i++) {
-            String a = dp[i - 1] + strings[i - 1];
-            String b = strings[i - 1] + dp[i - 1];
-            if (Integer.valueOf(a) <= Integer.valueOf(b)) dp[i] = a;
-            else dp[i] = b;
+            String b;
+            String a = dp[i - 1] + strings[i - 1];//a值表示i-1的最小值在前，最后一个字符在后的组合比如  1121 + 01
+            if (Integer.valueOf(strings[i - 1]) == 0)//如果最后一个字符取值是0，那么它在前的清空就是dp[i-1]
+                b = dp[i - 1];
+            else
+                b = strings[i - 1] + dp[i - 1];
+
+            //如果b为""表示前面的都是0的组合
+            if (b == "") {
+                if (Integer.valueOf(a) == 0) dp[i] = "";//同时a也是0.没得说继续""
+                else dp[i] = a;//否则为a
+            } else {//不为"" 那么就需要比较大小来定了
+                if (Integer.valueOf(a) <= Integer.valueOf(b)) dp[i] = a;
+                else dp[i] = b;
+            }
+
         }
-        System.out.println(Integer.valueOf(dp[strings.length]));
+        if (dp[strings.length] == "")
+            System.out.println("0");
+        else
+            System.out.println(Integer.valueOf(dp[strings.length]));
     }
 
 
     static int result = 0;
     static int count = 0;
     static int indexI = 0;
-    static int indexJ = 9;
+    static int indexJ = 0;
     static int n, m;
 
     public static void userSetZero() {
-        n = 3;
-        m = 3;
-        setZero(new int[][]{{1, 1, 1}, {0, 1, 0}, {0, 1, 0}});
+        int[][] inputs;
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        m = in.nextInt();
+        inputs = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                inputs[i][j] = in.nextInt();
+            }
+        }
+        //new int[][]{{1, 1, 1}, {0, 1, 0}, {0, 1, 0}}
+        // 1 1 1 1
+        // 0 0 0 1
+        // 1 1 0 0
+        // 1 1 0 1
+        setZero2(inputs);
         System.out.println("需要点击" + result + "次");
     }
 
@@ -115,6 +155,22 @@ public class CodeHuaWeiTest {
         //左下
         if (x + 1 < n && y - 1 >= 0 && inputs[x + 1][y - 1] == 1)
             setPointZero(x + 1, y - 1, inputs);
+    }
+
+    /**
+     * 卧槽 根本不需要判断哪里的1最多，直接碰到1递归归零，只要是连着的肯定都会归零;然后递归调用;
+     * @param inputs
+     */
+    public static void setZero2(int[][] inputs){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (inputs[i][j] == 1) {
+                    setPointZero(i, j, inputs);
+                    result++;
+//                    setZero(inputs);
+                }
+            }
+        }
     }
 }
 
